1f6e5e633b9bccc995fb786dc9f384392cd0a035
Aniket Patil <aniket.patil@livspace.com>
2020-01-13 16:12:52 +0530

    test

--- src/App.test.js
+++ src/App.test.js
@@ -9,3 +9,10 @@ test('renders learn react link', () => {
 });
 
 
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
